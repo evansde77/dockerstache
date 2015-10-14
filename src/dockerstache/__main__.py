8d029cc59b77b4490316ee3b2de69e383658e34e
Dave Evans <evansde77@gmail.com>
2015-10-14 14:25:59 -0500

    initial working cli

--- src/dockerstache/__main__.py
+++ src/dockerstache/__main__.py
@@ -4,10 +4,20 @@ _dockerstache_
 
 """
 import os
+import sys
 import argparse
+import json
+from .templates import process_templates
 
 
 def build_parser():
+    """
+    _build_parser_
+
+    Set up CLI parser options, parse the
+    CLI options an return the parsed results
+
+    """
     parser = argparse.ArgumentParser(
         description='Diesel docker templating util'
     )
@@ -23,12 +33,38 @@ def build_parser():
         dest='input',
         default=os.getcwd()
         )
+    parser.add_argument(
+        '--context', '-c',
+        help='JSON file containing context dictionary to render templates',
+        dest='context',
+        required=True
+    )
 
     opts = parser.parse_args()
     return opts
 
+
 def main():
+    """
+    _main_
+
+    Create a CLI parser and use that to run
+    the template rendering process
+
+    """
     opts = build_parser()
+    if not os.path.exists(opts.context):
+        print("Context file {} not found".format(opts.context))
+        sys.exit(1)
+
+    with open(opts.context, 'r') as handle:
+        context = json.load(handle)
+
+    process_templates(
+        opts.input,
+        opts.output,
+        context
+        )
 
 
 if __name__ == '__main__':
--- src/dockerstache/templates.py
+++ src/dockerstache/templates.py
@@ -7,6 +7,7 @@ Find templates, render templates etc
 """
 import os
 import functools
+import pystache
 
 
 def dir_visitor(dirname, callable):
@@ -14,8 +15,11 @@ def dir_visitor(dirname, callable):
     _dir_visitor_
 
     walk through all files in dirname, find
-    directories and call the callable on them
+    directories and call the callable on them.
 
+    :param dirname: Name of directory to start visiting,
+      all subdirs will be visited
+    :param callable: Callable invoked on each dir visited
     """
     callable(dirname)
     for obj in os.listdir(dirname):
@@ -29,6 +33,11 @@ def replicate_directory_tree(input_dir, output_dir):
     _replicate_directory_tree_
 
     clone dir structure under input_dir into output dir
+    All subdirs beneath input_dir will be created under
+    output_dir
+    :param input_dir: path to dir tree to be cloned
+    :param output_dir: path to new dir where dir structure will
+       be created
     """
     def transplant_dir(target, dirname):
         x = dirname.replace(input_dir, target)
@@ -47,6 +56,10 @@ def find_templates(input_dir):
 
     traverse the input_dir structure and return a list
     of template files ending with .mustache
+
+    :param input_dir: Path to start recursive search for
+       mustache templates
+    :returns: List of file paths corresponding to templates
     """
     templates = []
 
@@ -61,3 +74,39 @@ def find_templates(input_dir):
     )
     return templates
 
+
+def render_template(template_in, file_out, context):
+    """
+    _render_template_
+
+    Render a single template file, using the context provided
+    and write the file out to the location specified
+
+    #TODO: verify the template is completely rendered, no
+       missing values
+
+    """
+    renderer = pystache.Renderer()
+    result = renderer.render_path(template_in, context)
+    with open(file_out, 'w') as handle:
+        handle.write(result)
+
+
+def process_templates(input_dir, target_dir, context):
+    """
+    _process_templates_
+
+    Given the input dir containing a set of template,
+    clone the structure under that directory into the target dir
+    using the context to process any mustache templates that
+    are encountered
+
+    """
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+    replicate_directory_tree(input_dir, target_dir)
+    templates = find_templates(input_dir)
+    for templ in templates:
+        output_file = templ.replace(input_dir, target_dir)
+        output_file = output_file[:-len('.mustache')]
+        render_template(templ,  output_file, context)
