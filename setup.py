90c62639a30d7f6ac3c86a88c028f6c44983e4ce
Dave Evans <evansde77@gmail.com>
2015-10-14 15:55:48 -0500

    dont forget to include install_requires...

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@
 
 from setuptools import setup, find_packages
 
+requirements_file = open('requirements.txt')
+requirements = requirements_file.read().strip().split('\n')
+
 setup(name='dockerstache',
       version='0.0.1',
       description='Dockerfile mustache templating tools',
       author='Dave Evans',
       author_email='evansde77.github@gmail.com',
       url='https://github.com/evansde77/dockerstache',
+      install_requires=requirements,
       packages = find_packages('src'),
       package_dir={'': 'src'},
       entry_points = {
