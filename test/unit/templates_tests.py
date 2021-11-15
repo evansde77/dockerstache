ef11dad7d6d60c91eb1131edab519e255609b345
devans <dave.evans@imc.com>
2021-11-15 10:46:57 -0600

    switch from pystache to chevron for rendering, update some test code and fix warnings

--- test/unit/templates_tests.py
+++ test/unit/templates_tests.py
@@ -64,27 +64,27 @@ class TemplatesTests(unittest.TestCase):
         templ.replicate_directory_tree(self.tempdir, self.target_dir)
         expected_path1 = os.path.join(self.target_dir, 'dir1', 'dir2', 'dir3')
         expected_path2 = os.path.join(self.target_dir, 'dir1', 'dir4', 'dir5')
-        self.failUnless(os.path.exists(expected_path1))
-        self.failUnless(os.path.exists(expected_path2))
+        self.assertTrue(os.path.exists(expected_path1))
+        self.assertTrue(os.path.exists(expected_path2))
 
     def test_find_templates(self):
         """test find template function"""
         templates_found = templ.find_templates(self.tempdir)
         self.assertEqual(len(templates_found), 2)
-        self.failUnless(self.template1 in templates_found)
-        self.failUnless(self.template2 in templates_found)
+        self.assertTrue(self.template1 in templates_found)
+        self.assertTrue(self.template2 in templates_found)
 
     def test_render_template(self):
         """test render template call"""
         file_out = os.path.join(self.target_dir, 'template1.json')
         context = {'user': 'steve', 'group': 'vanhalen'}
         templ.render_template(self.template1, file_out, context)
-        self.failUnless(os.path.exists(file_out))
+        self.assertTrue(os.path.exists(file_out))
         with open(file_out, 'r') as handle:
             data = json.load(handle)
 
-        self.failUnless('template' in data)
-        self.failUnless('value' in data)
+        self.assertTrue('template' in data)
+        self.assertTrue('value' in data)
         self.assertEqual(data['template'], 1)
         self.assertEqual(data['value'], context['user'])
 
@@ -95,8 +95,8 @@ class TemplatesTests(unittest.TestCase):
         templ.process_templates(self.tempdir, self.target_dir, context)
         expected_path1 = os.path.join(self.target_dir, 'dir1', 'dir2', 'dir3')
         expected_path2 = os.path.join(self.target_dir, 'dir1', 'dir4', 'dir5')
-        self.failUnless(os.path.exists(expected_path1))
-        self.failUnless(os.path.exists(expected_path2))
+        self.assertTrue(os.path.exists(expected_path1))
+        self.assertTrue(os.path.exists(expected_path2))
 
         expected_file1 = os.path.join(
             self.target_dir, 'dir1', 'dir2', 'template1.json'
@@ -105,20 +105,20 @@ class TemplatesTests(unittest.TestCase):
             self.target_dir, 'dir1', 'dir4', 'dir5', 'template2.json'
         )
 
-        self.failUnless(os.path.exists(expected_file1))
-        self.failUnless(os.path.exists(expected_file2))
+        self.assertTrue(os.path.exists(expected_file1))
+        self.assertTrue(os.path.exists(expected_file2))
 
         with open(expected_file1, 'r') as handle:
             data1 = json.load(handle)
         with open(expected_file2, 'r') as handle:
             data2 = json.load(handle)
 
-        self.failUnless('template' in data1)
-        self.failUnless('value' in data1)
+        self.assertTrue('template' in data1)
+        self.assertTrue('value' in data1)
         self.assertEqual(data1['template'], 1)
         self.assertEqual(data1['value'], context['user'])
-        self.failUnless('template' in data2)
-        self.failUnless('value' in data2)
+        self.assertTrue('template' in data2)
+        self.assertTrue('value' in data2)
         self.assertEqual(data2['template'], 2)
         self.assertEqual(data2['value'], context['group'])
 
@@ -129,8 +129,8 @@ class TemplatesTests(unittest.TestCase):
         templ.process_copies(self.tempdir, self.target_dir, ['exclude-this.json'])
         expected_path1 = os.path.join(self.target_dir, 'dir1', 'dir2', 'dir3')
         expected_path2 = os.path.join(self.target_dir, 'dir1', 'dir4', 'dir5')
-        self.failUnless(os.path.exists(expected_path1))
-        self.failUnless(os.path.exists(expected_path2))
+        self.assertTrue(os.path.exists(expected_path1))
+        self.assertTrue(os.path.exists(expected_path2))
 
         expected_file1 = os.path.join(
             self.target_dir, 'dir1', 'dir2', 'template1.json'
